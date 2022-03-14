1dd6691feb193577282404e22e231fd3a9078c05
liondot <funfunjulia@gmail.com>
2022-03-14 17:46:47 +0900

    arrow up

--- main.js
+++ main.js
@@ -10,3 +10,21 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('bright');
     }
 })
+
+// btn scroll 
+const visualBtn = document.querySelector('.visual_btn');
+
+visualBtn.addEventListener('click', () => {
+    const scrollTo = document.querySelector('#contact')
+    scrollTo.scrollIntoView({behavior: 'smooth'})
+});
+
+// floating_menu scrollY 
+const ftMenu = document.querySelector('.floating_menu');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > mainVisualHeight / 2 ) {
+        ftMenu.classList.add('active');
+    } else {
+        ftMenu.classList.remove('active')
+    }
+})
