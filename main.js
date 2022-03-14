d196b28703a42480c3c3f5e22be2008604950ee2
liondot <funfunjulia@gmail.com>
2022-03-14 19:02:52 +0900

    slick slide setting reveiew, campaign

--- main.js
+++ main.js
@@ -28,3 +28,72 @@ document.addEventListener('scroll', () => {
         ftMenu.classList.remove('active')
     }
 })
+
+
+
+// campaign section slide 
+$('.campaign_cards').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+
+// review section slide
+$('.reviews').slick({
+    slide: 'div', 
+    infinite: true, 	 
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    speed: 600,
+    arrows: true, 
+    dots: true,
+    autoplay: true, 
+    autoplaySpeed: 10000, 
+    pauseOnHover: true, 
+    vertical: false, 
+    prevArrow: "<button type='button' class='slick-prev'>Previous</button>", 
+    nextArrow: "<button type='button' class='slick-next'>Next</button>", 
+    dotsClass: "slick-dots", 
+    draggable: true, 
+    responsive: [ 
+
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+    ]
+
+});
+
