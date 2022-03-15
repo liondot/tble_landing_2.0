ef6e7275e20cc1b8be021c46042f6141d1f7a714
liondot <funfunjulia@gmail.com>
2022-03-15 11:24:23 +0900

    contact form setting

--- main.js
+++ main.js
@@ -19,6 +19,14 @@ visualBtn.addEventListener('click', () => {
     scrollTo.scrollIntoView({behavior: 'smooth'})
 });
 
+// 모바일 .iqr btn 클릭시 스크롤 적용 
+const iqrBtn = document.querySelector ('.iqr')
+
+iqrBtn.addEventListener('click', () => {
+    const goto = document.querySelector('#contact')
+    goto.scrollIntoView({behavior : 'smooth'})
+})
+
 
 // floating_menu scrollY 
 const ftMenu = document.querySelector('.floating_menu');
@@ -72,8 +80,8 @@ $('.campaign_cards').slick({
 $('.reviews').slick({
     slide: 'div', 
     infinite: true, 	 
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     speed: 600,
     arrows: true, 
     dots: true,
@@ -99,4 +107,113 @@ $('.reviews').slick({
 
 
 
-// mobile js 
+
+//  고객 데이터 폼 영역 
+(function () {
+    function validEmail(email) {
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        return re.test(email);
+    }
+
+    function validateHuman(honeypot) {
+        if (honeypot) {
+            console.log("Robot Detected!");
+            return true;
+        } else {
+            console.log("Welcome Human!");
+        }
+    }
+
+    function getFormData(form) {
+        var elements = form.elements;
+
+        var fields = Object.keys(elements).filter(function (k) {
+            return (elements[k].name !== "honeypot");
+        }).map(function (k) {
+            if (elements[k].name !== undefined) {
+                return elements[k].name;
+            } else if (elements[k].length > 0) {
+                return elements[k].item(0).name;
+            }
+        }).filter(function (item, pos, self) {
+            return self.indexOf(item) == pos && item;
+        });
+
+        var formData = {};
+        fields.forEach(function (name) {
+            var element = elements[name];
+            formData[name] = element.value;
+            if (element.length) {
+                var data = [];
+                for (var i = 0; i < element.length; i++) {
+                    var item = element.item(i);
+                    if (item.checked || item.selected) {
+                        data.push(item.value);
+                    }
+                }
+                formData[name] = data.join(', ');
+            }
+        });
+
+        // add form-specific values into the data
+        formData.formDataNameOrder = JSON.stringify(fields);
+        formData.formGoogleSheetName = form.dataset.sheet || "sheet2"; // default sheet name
+        formData.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+
+        console.log(formData);
+        return formData;
+    }
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        var form = event.target;
+        var data = getFormData(form);
+        if (data.email && !validEmail(data.email)) {
+            var invalidEmail = form.querySelector(".email-invalid");
+            if (invalidEmail) {
+                invalidEmail.style.display = "block";
+                return false;
+            }
+        } else {
+            disableAllButtons(form);
+            var url = form.action;
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', url);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function () {
+                console.log(xhr.status, xhr.statusText);
+                console.log(xhr.responseText);
+                var formElements = form.querySelector(".form-elements")
+                if (formElements) {
+                    formElements.style.display = "none"; // hide form
+                }
+                var thankYouMessage = form.querySelector(".thankyou_message");
+                if (thankYouMessage) {
+                    thankYouMessage.style.display = "block";
+                }
+                return;
+            };
+
+            var encoded = Object.keys(data).map(function (k) {
+                return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+            }).join('&');
+            xhr.send(encoded);
+        }
+    }
+
+    function loaded() {
+        console.log("Contact form submission handler loaded successfully.");
+        var forms = document.querySelectorAll("form.gform");
+        for (var i = 0; i < forms.length; i++) {
+            forms[i].addEventListener("submit", handleFormSubmit, false);
+        }
+    };
+    document.addEventListener("DOMContentLoaded", loaded, false);
+
+    function disableAllButtons(form) {
+        var buttons = form.querySelectorAll("button");
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
+        }
+    }
+})();
